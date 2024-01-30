fab7a95e7ab442df6ca8fc09fed9a30ae8fa0a02
Ryunosuke Hayashi <ryunosuke.engineer@gmail.com>
2024-01-31 08:56:27 +0900

    push

--- app/routes/caches.no-store.tsx
+++ app/routes/caches.no-store.tsx
@@ -1,6 +1,6 @@
 import type { HeadersFunction, LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData, useNavigate } from "@remix-run/react";
+import { useLoaderData  } from "@remix-run/react";
 
 /**
  * レスポンスヘッダーを設定する
@@ -22,10 +22,8 @@ export const loader: LoaderFunction = async () => {
 
 export default function Cache() {
   const { serverDate } = useLoaderData<typeof loader>();
-  const navigate = useNavigate();
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-5">
-      <button onClick={() => navigate(".", { replace: true })}>Refresh</button>
       <p>
         Cache-Control: no-store
         <br />
