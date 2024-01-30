15f93b933e6570e26def0947eb699dc670601569
Ryunosuke Hayashi <ryunosuke.engineer@gmail.com>
2024-01-31 08:44:06 +0900

    push

--- app/routes/caches.private.tsx
+++ app/routes/caches.private.tsx
@@ -1,6 +1,6 @@
 import type { HeadersFunction, LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useNavigate } from "@remix-run/react";
 
 /**
  * レスポンスヘッダーを設定する
@@ -21,10 +21,12 @@ export const loader: LoaderFunction = async () => {
 
 export default function Cache() {
   const { serverDate } = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-5">
+      <button onClick={() => navigate(".", { replace: true })}>Refresh</button>
       <p>
-        Cache-Control: private, max-age=100
+        Cache-Control: private, max-age=10
         <br />
         ローカルキャッシュが返却される。キャッシュは10秒で失効する。
       </p>
