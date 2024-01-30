4a4b4aeb7804d1e09995540637ccb73050eeb6ce
Ryunosuke Hayashi <ryunosuke.engineer@gmail.com>
2024-01-30 09:50:35 +0900

    修正

--- app/routes/caches.public.tsx
+++ app/routes/caches.public.tsx
@@ -4,7 +4,7 @@ import { useLoaderData } from "@remix-run/react";
 
 /**
  * レスポンスヘッダーを設定する
- * @returns 
+ * @returns
  */
 export const headers: HeadersFunction = () => ({
   "Cache-Control": "public, max-age=3600, s-maxage=3600",
@@ -12,17 +12,17 @@ export const headers: HeadersFunction = () => ({
 
 /**
  * 画面読み込みの際のサーバーサイドの処理
- * @returns 
+ * @returns
  */
 export const loader: LoaderFunction = async () => {
   const serverDate = new Date().toString();
   return json({ serverDate });
 };
 
+const clientDate = new Date().toString();
 
 export default function Cache() {
   const { serverDate } = useLoaderData<typeof loader>();
-  const clientDate = new Date().toString();
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-5">
       <div>クライアント</div>
